c8d64d0db6ccb80e2112d6b303f10c8c387e48e0
Nicholas Veras <nicholasveras@MacBook-Air-de-Nicholas.local>
2024-02-27 13:02:28 -0300

    feat: create loading and components

--- src/Listagem.js
+++ src/Listagem.js
@@ -3,14 +3,24 @@ import { View, Text, StyleSheet } from "react-native";
 
 export default function Listagem({data}){
   return(
-    <View>
-      <Text>{data.nome}</Text>
-      <Text>{data.cargo}</Text>
+    <View style={styles.container}>
+      <Text style={styles.text}>{data.nome}</Text>
+      <Text style={styles.text}>{data.cargo}</Text>
     </View>
   )
 }
 
 
 const styles = StyleSheet.create({
-
+  container: {
+    flex: 1,
+    marginTop: 10,
+    marginBottom: 5,
+    padding: 10,
+    backgroundColor: '#121212',
+  },
+  text: {
+    color: '#FFF',
+    fontSize: 17,
+  }
 });
